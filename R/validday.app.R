ed0a6b4f6cd1bae17052d0693b873d1d165149dc
Annelinde Lettink <annelinde.lettink@gmail.com>
2024-01-10 11:40:58 +0100

    function description and take into account lost 5 minutes as app registers untill 23.55

--- R/validday.app.R
+++ R/validday.app.R
@@ -1,13 +1,25 @@
 #' validday.app
 #'
-#' @description 'validday.app' Preprocesses the app data with the activity duration per category per day by removing invalid days
+#' @description 'validday.app' preprocesses the app data with the activity duration per category per day by removing invalid days
 #'
-#' @param data data.frame object that contains the app data
-#' @param minhours Integer that indicates the minimal recorded hours/day for a day to be considered a valid day, default = 8.
-#' @param mindays Integer that indicates the minimal number of days to be considered valid data, default = 7.
+#' @param data A data.frame object that contains the app data
+#' @param minhours An integer indicating the minimal recorded hours/day for a day to be considered a valid day (default = 8).
+#' @param mindays An integer that indicates the minimal number of days to be considered valid data (default = 7).
 #' @param savedir Path to the directory where the preprocessed valid data will be stored.
-
+#' 
+#' @return A data.frame containing the valid preprocessed data based on the specified criteria.
+#' 
 #' @export
+#' 
+#' @examples
+#' # Example usage:
+#' # validday.app(data = my_data, minhours = 6, mindays = 5, savedir = "path/to/save")
+#'
+#' @details
+#' This function preprocesses app data, removing days with insufficient activity duration per day.
+#' It filters data based on the given minimum recorded hours/day and minimum number of valid days.
+#' The resulting valid data will be saved in the specified directory.
+#'
 validday.app <- function(data, minhours = 8, mindays = 7, savedir){
   
   participants <- unique(data$castorID)
@@ -18,7 +30,7 @@ validday.app <- function(data, minhours = 8, mindays = 7, savedir){
       tmp_valid <- data.frame()
       for(day in 1:nrow(tmp)){
         tmp.day <- tmp[day,]
-        if(sum(tmp.day$PA,tmp.day$SB,tmp.day$sleep, na.rm = TRUE) >= minhours * 60){
+        if(sum(tmp.day$PA,tmp.day$SB,tmp.day$sleep, 5, na.rm = TRUE) >= minhours * 60){
           tmp_valid <- rbind(tmp_valid, tmp.day)
         }
       }
