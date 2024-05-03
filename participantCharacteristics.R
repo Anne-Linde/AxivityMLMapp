09d17c55c48183be5927ac16f9e1e7a9bb0bf5f8
Annelinde Lettink <annelinde.lettink@gmail.com>
2024-05-03 11:05:18 +0200

    Update demographics

--- participantCharacteristics.R
+++ participantCharacteristics.R
@@ -1,9 +1,9 @@
 # Participant characteristics
-date <- "20231031" # Date of last data update
+date <- "20240423" # Date of last data update
 
 # Load demographic data
 path.demo <- "/Users/annelindelettink/Documents/Work MacBook Pro Annelinde/My Little Moves (MLM)/Demographics/"
-file.demo <- "demografische_gegevens_minimal_with_zbmi.csv"
+file.demo <- paste0(date, "_demografische_gegevens_minimal_with_zbmi.csv")
 demographics <- read.csv(paste0(path.demo, file.demo), sep = ",")[,-1]
 
 ###### MLM app reliability
@@ -13,60 +13,61 @@ file.minimal.app <- "valid_data_12h2d.RData"
 load(paste0(path.app, file.minimal.app))
 pp.app.rel <- unique(valid_data$castorID)
 length(pp.app.rel)
-demo.rel.app <- demographics[which(demographics$id %in% pp.app.rel,),]
+demo.rel.app <- demographics[which(demographics$castorID %in% pp.app.rel,),]
 
-# 6 Parents participated with two children
-# 110316 (same as 110315)
-demo.rel.app[demo.rel.app$id == "110316", c(8, 9, 10, 11, 12, 13, 14, 15, 16)] <- NA
+# 7 Parents participated with two children
+# 110069 (same as 110108)
+# 110224 (same as 110143)
+# 110315 (same as 110316)
 # 110416 (same as 110415)
-demo.rel.app[demo.rel.app$id == "110416", c(8, 9, 10, 11, 12, 13, 14, 15, 16)] <- NA
-#11043 (same as 110224) -> respondent is missing, we know that this is a male
-demo.rel.app[demo.rel.app$id == "110143", c(8)] <- 1
-# 98MLM003 (same as 98MLM002), 98MLM030 (same as 98MLM022), 98MLM051 (same as 98MLM049)
+# 98MLM003 (same as 98MLM002)
+# 98MLM030 (same as 98MLM022)
+# 98MLM051 (same as 98MLM049)
+demo.rel.app[demo.rel.app$castorID == "110069", c("respondent", "gender_respondent", "birth_country_bio_mother", "birth_country_bio_father", 
+                                                  "dob_respondent", "age_years_respondent_parent1", "education_respondent_parent1", "education_partner")] <- NA
+demo.rel.app[demo.rel.app$castorID == "110224", c("respondent", "gender_respondent", "birth_country_bio_mother", "birth_country_bio_father", 
+                                                  "dob_respondent", "age_years_respondent_parent1", "education_respondent_parent1", "education_partner")] <- NA
+demo.rel.app[demo.rel.app$castorID == "110315", c("respondent", "gender_respondent", "birth_country_bio_mother", "birth_country_bio_father", 
+                                                  "dob_respondent", "age_years_respondent_parent1", "education_respondent_parent1", "education_partner")] <- NA
+demo.rel.app[demo.rel.app$castorID == "110416", c("respondent", "gender_respondent", "birth_country_bio_mother", "birth_country_bio_father", 
+                                                  "dob_respondent", "age_years_respondent_parent1", "education_respondent_parent1", "education_partner")] <- NA
+demo.rel.app[demo.rel.app$castorID == "98MLM003", c("respondent", "gender_respondent", "birth_country_bio_mother", "birth_country_bio_father", 
+                                                  "dob_respondent", "age_years_respondent_parent1", "education_respondent_parent1", "education_partner")] <- NA
+demo.rel.app[demo.rel.app$castorID == "98MLM030", c("respondent", "gender_respondent", "birth_country_bio_mother", "birth_country_bio_father", 
+                                                    "dob_respondent", "age_years_respondent_parent1", "education_respondent_parent1", "education_partner")] <- NA
+demo.rel.app[demo.rel.app$castorID == "98MLM051", c("respondent", "gender_respondent", "birth_country_bio_mother", "birth_country_bio_father", 
+                                                    "dob_respondent", "age_years_respondent_parent1", "education_respondent_parent1", "education_partner")] <- NA
 
-# 103 missings, TO DO: check who we need to (re)send the questionnaire!!!
-#calculate age, gender distribution, education distribution. #missing data?
-age_parent <- c()
-education <- c()
-for(subj in 1:nrow(demo.rel.app)){
-  if(demo.rel.app$id[subj] == "02MLM001"){
-    age_parent <- c(age_parent, NA)
-  } else if(!is.na(demo.rel.app$respondent[subj]) & demo.rel.app$respondent[subj] == "0"){
-    age_parent <- c(age_parent, demo.rel.app$leeftijd_jaren_bio_moeder[subj])
-    education <- c(education, demo.rel.app$opleiding_bio_moeder[subj])
-    
-  } else if(!is.na(demo.rel.app$respondent[subj]) & demo.rel.app$respondent[subj] == "1"){
-    age_parent <- c(age_parent, demo.rel.app$leeftijd_jaren_bio_vader[subj])
-    education <- c(education, demo.rel.app$opleiding_bio_vader[subj])
-  } else{
-    age_parent <- c(age_parent, NA)
-    education <- c(education, NA)
-  }
-}
-sum(is.na(age_parent)) #114 missende waarden
-mean(age_parent, na.rm = TRUE)
-sd(age_parent, na.rm = TRUE)
+# Age parent
+sum(is.na(demo.rel.app$age_years_respondent_parent1)) #51 - 7 = 44 missing values
+mean(demo.rel.app$age_years_respondent_parent1, na.rm = TRUE)
+sd(demo.rel.app$age_years_respondent_parent1, na.rm = TRUE)
 
 # Calulate gender distribution
+sum(is.na(demo.rel.app$respondent)) #8 - 7 = 1 missing values
 table(as.factor(demo.rel.app$respondent))
-prop.table(table(as.factor(demo.rel.app$respondent))) * 100 #86.7 female
+prop.table(table(as.factor(demo.rel.app$respondent))) * 100 #91.1 female
+
 # Calulate birth country mother and father
-table(demo.rel.app$geboorteland_bio_moeder)
-sum(table(demo.rel.app$geboorteland_bio_moeder)) # total 220 values, 220-1794
-table(demo.rel.app$geboorteland_bio_vader)
-sum(table(demo.rel.app$geboorteland_bio_vader)) # total 218 values, 218-181
+sum(is.na(demo.rel.app$birth_country_bio_mother)) #34 - 7 = 27 missing values
+table(demo.rel.app$birth_country_bio_mother)
+sum(table(demo.rel.app$birth_country_bio_mother)) # 290-227 = 63 niet in NL
+sum(is.na(demo.rel.app$birth_country_bio_father)) #43 - 7 = 36 missing values
+table(demo.rel.app$birth_country_bio_father)
+sum(table(demo.rel.app$birth_country_bio_father)) # 281-227 = 54 niet in NL
+
 #Education
-table(education)
+sum(is.na(demo.rel.app$education_respondent_parent1)) #44+13 - 7 = 50 missing values
+table(demo.rel.app$education_respondent_parent1)
+sum(table(demo.rel.app$education_respondent_parent1)) #280+12-6
 
 ## Child
 # age
-mean(demo.rel.app$age_in_months_meting_1)
-sd(demo.rel.app$age_in_months_meting_1)
+mean(demo.rel.app$age_in_months_measurement_period_1, na.rm = T)
+sd(demo.rel.app$age_in_months_measurement_period_1, na.rm = T)
 # gender 2 = girl
-table(as.factor(demo.rel.app$geslacht_kind))
-prop.table(table(as.factor(demo.rel.app$geslacht_kind))) * 100 #48.8. girl
-
-
+table(as.factor(demo.rel.app$gender_child))
+prop.table(table(as.factor(demo.rel.app$gender_child))) * 100 #48.8. girl
 
 ###### Accelerometer reliability
 #12h2days --> minimal set
@@ -77,57 +78,58 @@ wrist <- axivity.estimates
 rm(axivity.estimates)
 pp <- unique(c(hip$id, wrist$id))
 length(pp)
-demo.rel.acc <- demographics[which(demographics$id %in% pp,),]
+demo.rel.acc <- demographics[which(demographics$castorID %in% pp,),]
 
 # 3 Parents participated with two children
-# 98MLM003 (same as 98MLM002), 98MLM030 (same as 98MLM022), 98MLM051 (same as 98MLM049)
+# 98MLM003 (same as 98MLM002)
+# 98MLM030 (same as 98MLM022)
+# 98MLM051 (same as 98MLM049)
+demo.rel.acc[demo.rel.acc$castorID == "98MLM003", c("respondent", "gender_respondent", "birth_country_bio_mother", "birth_country_bio_father", 
+                                                    "dob_respondent", "age_years_respondent_parent1", "education_respondent_parent1", "education_partner")] <- NA
+demo.rel.acc[demo.rel.acc$castorID == "98MLM030", c("respondent", "gender_respondent", "birth_country_bio_mother", "birth_country_bio_father", 
+                                                    "dob_respondent", "age_years_respondent_parent1", "education_respondent_parent1", "education_partner")] <- NA
+demo.rel.acc[demo.rel.acc$castorID == "98MLM051", c("respondent", "gender_respondent", "birth_country_bio_mother", "birth_country_bio_father", 
+                                                    "dob_respondent", "age_years_respondent_parent1", "education_respondent_parent1", "education_partner")] <- NA
 
-#calculate age, gender distribution, education distribution. #missing data?
-age_parent <- c()
-education <- c()
-for(subj in 1:nrow(demo.rel.acc)){
-  if(demo.rel.acc$id[subj] == "02MLM001"){
-    age_parent <- c(age_parent, NA)
-  } else if(!is.na(demo.rel.acc$respondent[subj]) & demo.rel.acc$respondent[subj] == "0"){
-    age_parent <- c(age_parent, demo.rel.acc$leeftijd_jaren_bio_moeder[subj])
-    education <- c(education, demo.rel.acc$opleiding_bio_moeder[subj])
-    
-  } else if(!is.na(demo.rel.acc$respondent[subj]) & demo.rel.acc$respondent[subj] == "1"){
-    age_parent <- c(age_parent, demo.rel.acc$leeftijd_jaren_bio_vader[subj])
-    education <- c(education, demo.rel.acc$opleiding_bio_vader[subj])
-  } else{
-    age_parent <- c(age_parent, NA)
-    education <- c(education, NA)
-  }
-}
-sum(is.na(age_parent)) #14 missende waarden
-mean(age_parent, na.rm = TRUE)
-sd(age_parent, na.rm = TRUE)
+
+# Age parent
+sum(is.na(demo.rel.acc$age_years_respondent_parent1)) #10 - 3 = 7 missing values
+mean(demo.rel.acc$age_years_respondent_parent1, na.rm = TRUE)
+sd(demo.rel.acc$age_years_respondent_parent1, na.rm = TRUE)
 
 # Calulate gender distribution
+sum(is.na(demo.rel.acc$respondent)) #3 - 3 = 0 missing values
 table(as.factor(demo.rel.acc$respondent))
-prop.table(table(as.factor(demo.rel.acc$respondent))) * 100 #85.7 female
+prop.table(table(as.factor(demo.rel.acc$respondent))) * 100 #87.5 female
+
 # Calulate birth country mother and father
-table(demo.rel.acc$geboorteland_bio_moeder)
-sum(table(demo.rel.acc$geboorteland_bio_moeder)) # total 63 values, 63-50
-table(demo.rel.acc$geboorteland_bio_vader)
-sum(table(demo.rel.acc$geboorteland_bio_vader)) # total 63 values, 63-181
+sum(is.na(demo.rel.acc$birth_country_bio_mother)) #9 - 3 = 6 missing values
+table(demo.rel.acc$birth_country_bio_mother)
+sum(table(demo.rel.acc$birth_country_bio_mother)) # 66-53 = 13 niet in NL
+sum(is.na(demo.rel.acc$birth_country_bio_father)) #9 - 3 = 6 missing values
+table(demo.rel.acc$birth_country_bio_father)
+sum(table(demo.rel.acc$birth_country_bio_father)) # 66-53 = 13 niet in NL
+
 #Education
-table(education)
+sum(is.na(demo.rel.acc$education_respondent_parent1)) #9 - 3 = 6 missing values
+table(demo.rel.acc$education_respondent_parent1)
+sum(table(demo.rel.acc$education_respondent_parent1)) #280+12-6
 
 ## Child
 # age
-mean(demo.rel.acc$age_in_months_meting_1)
-sd(demo.rel.acc$age_in_months_meting_1)
+mean(demo.rel.acc$age_in_months_measurement_period_1, na.rm = T)
+sd(demo.rel.acc$age_in_months_measurement_period_1, na.rm = T)
 # gender 2 = girl
-table(as.factor(demo.rel.acc$geslacht_kind))
-prop.table(table(as.factor(demo.rel.acc$geslacht_kind))) * 100 #45.3. girl
+table(as.factor(demo.rel.acc$gender_child))
+prop.table(table(as.factor(demo.rel.acc$gender_child))) * 100 #45.3. girl
+
 #zbmi
 mean(demo.rel.acc$zbmi, na.rm = T)
 sd(demo.rel.acc$zbmi, na.rm = T)
 
 
 ###### MLM app - accelerometers overlap (cohort 3 only)
+date <- "20231031" # App data until measurement period 1 is sufficient
 load(paste0("/Users/annelindelettink/Documents/Work MacBook Pro Annelinde/My Little Moves (MLM)/Comparison MLM-app and accelerometer data/Analyses/output/app_ax_entry_" , date, ".RData"))
 # Delete rows for which no acceleration data is there
 index_remove <- which(is.na(data.pp$ENMO.hip & data.pp$ENMO.wrist&data.pp$MAD.hip & data.pp$MAD.wrist))
@@ -135,70 +137,67 @@ data.pp <- data.pp[-index_remove,]
 pp <- unique(data.pp$castorID)
 length(pp)
 
-#72 hip, 70 wrist
-sum(wrist.enmo$castorID %in% hip.enmo$castorID == FALSE) # = 0, all wrist acc also have hip
-sum(hip.enmo$castorID %in% wrist.enmo$castorID == FALSE) # 2 only hip and no wrist
-demo.consistency <- demographics[which(demographics$id %in% pp),]
-# 98MLM003 (same as 98MLM002), 98MLM026 (same as 98MLM025), 98MLM030 (same as 98MLM022), 98MLM051 (same as 98MLM049)
-#remove_id <- c("98MLM003", "98MLM026")
+# #72 hip, 70 wrist
+# sum(wrist.enmo$castorID %in% hip.enmo$castorID == FALSE) # = 0, all wrist acc also have hip
+# sum(hip.enmo$castorID %in% wrist.enmo$castorID == FALSE) # 2 only hip and no wrist
+# demo.consistency <- demographics[which(demographics$id %in% pp),]
 
-## Parent
-# Calculate age
-# birthdate mother 02MLM001: strange 1077-05-21 -> removed
-age_parent <- c()
-education <- c()
-for(subj in 1:nrow(demo.consistency)){
-  if(demo.consistency$id[subj] == "02MLM001"){
-    age_parent <- c(age_parent, NA)
-  } else if(!is.na(demo.consistency$respondent[subj]) & demo.consistency$respondent[subj] == "0"){
-    age_parent <- c(age_parent, demo.consistency$leeftijd_jaren_bio_moeder[subj])
-    education <- c(education, demo.consistency$opleiding_bio_moeder[subj])
-    
-  } else if(!is.na(demo.consistency$respondent[subj]) & demo.consistency$respondent[subj] == "1"){
-    age_parent <- c(age_parent, demo.consistency$leeftijd_jaren_bio_vader[subj])
-    education <- c(education, demo.consistency$opleiding_bio_vader[subj])
-  } else{
-    age_parent <- c(age_parent, NA)
-    education <- c(education, NA)
-  }
-}
-sum(is.na(age_parent)) #14 missende waarden
-mean(age_parent, na.rm = TRUE)
-sd(age_parent, na.rm = TRUE)
+demo.rel.acc.app <- demographics[which(demographics$castorID %in% pp,),]
+
+# 3 Parents participated with two children
+# 98MLM003 (same as 98MLM002)
+# 98MLM030 (same as 98MLM022)
+# 98MLM051 (same as 98MLM049)
+demo.rel.acc.app[demo.rel.acc.app$castorID == "98MLM003", c("respondent", "gender_respondent", "birth_country_bio_mother", "birth_country_bio_father", 
+                                                    "dob_respondent", "age_years_respondent_parent1", "education_respondent_parent1", "education_partner")] <- NA
+demo.rel.acc.app[demo.rel.acc.app$castorID == "98MLM030", c("respondent", "gender_respondent", "birth_country_bio_mother", "birth_country_bio_father", 
+                                                    "dob_respondent", "age_years_respondent_parent1", "education_respondent_parent1", "education_partner")] <- NA
+demo.rel.acc.app[demo.rel.acc.app$castorID == "98MLM051", c("respondent", "gender_respondent", "birth_country_bio_mother", "birth_country_bio_father", 
+                                                    "dob_respondent", "age_years_respondent_parent1", "education_respondent_parent1", "education_partner")] <- NA
+
+# Age parent
+sum(is.na(demo.rel.acc.app$age_years_respondent_parent1)) #10 - 3 = 7 missing values
+mean(demo.rel.acc.app$age_years_respondent_parent1, na.rm = TRUE)
+sd(demo.rel.acc.app$age_years_respondent_parent1, na.rm = TRUE)
 
 # Calulate gender distribution
-table(as.factor(demo.consistency$respondent))
-prop.table(table(as.factor(demo.consistency$respondent))) * 100 #86.7 female
+sum(is.na(demo.rel.acc.app$respondent)) #3 - 3 = 0 missing values
+table(as.factor(demo.rel.acc.app$respondent))
+prop.table(table(as.factor(demo.rel.acc.app$respondent))) * 100 #88.4 female
 
 # Calulate birth country mother and father
-sum(table(demo.consistency$geboorteland_bio_moeder))
-table(demo.consistency$geboorteland_bio_moeder)
-sum(table(demo.consistency$geboorteland_bio_vader))
-
-table(demo.consistency$geboorteland_bio_vader)
+sum(is.na(demo.rel.acc.app$birth_country_bio_mother)) #9 - 3 = 6 missing values
+table(demo.rel.acc.app$birth_country_bio_mother)
+sum(table(demo.rel.acc.app$birth_country_bio_mother)) # 63-47 = 16 niet in NL
+sum(is.na(demo.rel.acc.app$birth_country_bio_father)) #9 - 3 = 6 missing values
+table(demo.rel.acc.app$birth_country_bio_father)
+sum(table(demo.rel.acc.app$birth_country_bio_father)) # 63-50 = 13 niet in NL
 
 #Education
-table(education)
+sum(is.na(demo.rel.acc.app$education_respondent_parent1)) #9 - 3 = 6 missing values
+table(demo.rel.acc.app$education_respondent_parent1)
+sum(table(demo.rel.acc.app$education_respondent_parent1)) #63-8=55
 
 ## Child
 # age
-mean(demo.consistency$age_in_months_meting_1) 
-sd(demo.consistency$age_in_months_meting_1) 
+mean(demo.rel.acc.app$age_in_months_measurement_period_1, na.rm = T)
+sd(demo.rel.acc.app$age_in_months_measurement_period_1, na.rm = T)
 # gender 2 = girl
-table(as.factor(demo.consistency$geslacht_kind))
-prop.table(table(as.factor(demo.consistency$geslacht_kind))) * 100 #44.4 girl
+table(as.factor(demo.rel.acc.app$gender_child))
+prop.table(table(as.factor(demo.rel.acc.app$gender_child))) * 100 #45.8. girl
+
 #zbmi
-mean(demo.consistency$zbmi, na.rm = T)
-sd(demo.consistency$zbmi, na.rm = T)
+mean(demo.rel.acc.app$zbmi, na.rm = T)
+sd(demo.rel.acc.app$zbmi, na.rm = T)
 
 
-
-##### Flowchart
-file.app.activities <- "20230918_MLMapp_activiteit_gedrag_duur_per_dag.csv"
-
-
-load(paste0(path.app, file.app.activities))
-
-
+# 
+# ##### Flowchart
+# file.app.activities <- "20230918_MLMapp_activiteit_gedrag_duur_per_dag.csv"
+# 
+# 
+# load(paste0(path.app, file.app.activities))
+# 
+# 
 
 
